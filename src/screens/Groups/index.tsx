9319cb41be14a6fef9a1df4bbf3cd61954d7a7dc
Lucas Diniz Almeida <almeidalucas820@gmail.com>
2023-04-14 13:30:27 -0300

    feat: create GroupCard and ListEmpty components
    This commit adds the following changes to the code:
    
    Implementation of GroupCard component
    Implementation of ListEmpty component
    The GroupCard component is responsible for displaying information related to a specific group, while the ListEmpty component displays a message when a list is empty.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,14 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { FlatList } from 'react-native';
+import { useState } from 'react';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header />
@@ -10,6 +16,26 @@ export function Groups() {
         title='Turmas'
         subtitle='Jogue com a sua turma'
       />
+
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Que tal cadastrar a primeira turna'
+          />
+        )}
+      />
+
+
+
     </Container>
   );
 }
